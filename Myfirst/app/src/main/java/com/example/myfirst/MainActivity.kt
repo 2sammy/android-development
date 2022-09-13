1c5170cf0e6e964ae88c9e6c05a775111eaee8ea
2sammy <samuelmanoah61@gmail.com>
2022-09-13 12:31:22 +0300

    my first app

---
+++ Myfirst/app/src/main/java/com/example/myfirst/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    onCreate(savedInstancesState: Bundle?) {
+super.onCreate(savedInstanceState)
+    }
+
+
+}
